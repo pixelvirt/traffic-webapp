113ded119e0515155c1baa181d0a941df3f783d9
shahiutsav <92775677+shahiutsav@users.noreply.github.com>
2024-02-08 11:10:23 +0545

    Add breadcrumbs to the junctions layout
    This commit adds breadcrumbs to the layout of the junctions page. This
    change makes it easy for the user to view where they are in the
    application.

--- src/app/junctions/layout.tsx
+++ src/app/junctions/layout.tsx
@@ -1,3 +1,4 @@
+import BreadCrumbs from '@/components/Breadcrumb'
 import Sidebar from '@/components/sidebar/Sidebar'
 
 export default async function Layouts({
@@ -8,7 +9,17 @@ export default async function Layouts({
   return (
     <div className='flex'>
       <Sidebar />
-      {children}
+      <div className='flex w-full flex-col'>
+        <BreadCrumbs
+          homeElement={'Home'}
+          separator={<span> &gt; </span>}
+          activeClasses='text-white font-bold'
+          containerClasses='flex py-5 pl-4'
+          listClasses='hover:underline mx-2 fon-bold'
+          capitalizeLinks
+        />
+        {children}
+      </div>
     </div>
   )
 }
