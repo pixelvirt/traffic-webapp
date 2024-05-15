d88587da2dcf5fca1d25f9a6fe26e034bf3ff935
shahiutsav <92775677+shahiutsav@users.noreply.github.com>
2024-05-15 13:16:52 +0545

    Dockerize the application

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: '192.168.101.202',
-        port: '5001',
-        pathname: '/media/violation/**',
+        protocol: "http",
+        hostname: "192.168.101.202",
+        port: "5001",
+        pathname: "/media/violation/**",
       },
     ],
   },
